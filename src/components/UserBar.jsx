8038dce5a617d1424c2252d4162452a85ac85a33
sabifromtherock <szabolcs.sziklai@protonmail.com>
2023-04-21 18:12:40 +0100

    add logout button

--- src/components/UserBar.jsx
+++ src/components/UserBar.jsx
@@ -1,10 +1,18 @@
-const UserBar = ({ user }) => {
+const UserBar = ({ user, setUser }) => {
+  const handleLogout = (event) => {
+    event.preventDefault();
+    setUser("");
+  };
+
   return (
     <div className="userContainer">
       {user ? (
         <p className="headerUsername">
           Welcome back <br />
           <span>{user}</span>
+          <button type="button" onClick={handleLogout}>
+            Logout
+          </button>
         </p>
       ) : null}
     </div>
